48f09478353fe67a29539c168842bc1a70613038
kouyama <guruguru230203@gmail.com>
2015-04-23 14:04:35 +0900

    dbの作成

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages" do
 
     it { should have_content('Sample3 App') }
     it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it { should_not have_title('Home') }
   end
 
   describe "Help page" do
@@ -31,5 +31,33 @@ describe "Static pages" do
 
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))# ここにコードを記入
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up')) # ここにコードを記入
+    click_link "sample3 app"
+   # expect(page).to have_title(full_title('Sample3 App')) # ここにコードを記入
+  end
+   describe "full_title" do
+    it "should include the page title" do
+      expect(full_title("foo")).to match(/foo/)
+    end
+
+    it "should include the base title" do
+      expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample3 App/)
+    end
+
+    it "should not include a bar for the home page" do
+      expect(full_title("")).not_to match(/\|/)
+    end
   end
 end
