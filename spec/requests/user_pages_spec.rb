a6c5f43363790d36142bcebd38a0638424fd5a33
ruby-v <guruguru230203@gmil.com>
2015-04-25 23:22:27 +0900

    ユーザー登録成功

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,11 +6,24 @@ describe "User pages" do
 
   describe "signup page" do
     before { visit signup_path }
-    let(:submit) { "Create my account" }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
+
+  describe "profile page" do
+  # ユーザー変数を作成するためのコードに置き換える。
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       it "should not create a user" do
@@ -30,14 +43,5 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-
-
-  describe "profile page" do
-  # ユーザー変数を作成するためのコードに置き換える。
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
   end
 end
